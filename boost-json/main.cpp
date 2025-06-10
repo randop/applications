1cc6d55da5d8ec9111dbe3d4c0370a65e7048586
Randolph Ledesma <randopledesma+gitlab@hotmail.com>
2025-06-10 13:16:46 +0000

    + Formats source files of boost json app

--- boost-json/main.cpp
+++ boost-json/main.cpp
@@ -1,23 +1,24 @@
+#include <boost/json/src.hpp>
 #include <iostream>
 #include <string>
-#include <boost/json/src.hpp>
 
 using namespace boost::json;
 
 int main(int argc, char **argv) {
-    // Testing some type confusion issues
-    std::string experiment = "{\"description\": \"Float (exp)\", \"test\": 1E400}";
-    value test = parse(experiment);
-    std::string real = serialize(test);
-    std::cout << "[!] EXPECTED: " << experiment << std::endl;
-    std::cout << "[-] Result: " << real << std::endl;
+  // Testing some type confusion issues
+  std::string experiment =
+      "{\"description\": \"Float (exp)\", \"test\": 1E400}";
+  value test = parse(experiment);
+  std::string real = serialize(test);
+  std::cout << "[!] EXPECTED: " << experiment << std::endl;
+  std::cout << "[-] Result: " << real << std::endl;
 
-    /***
-     * program output:
+  /***
+   * program output:
 
-    [!] EXPECTED: {"description": "Float (exp)", "test": 1E400}
-    [-] Result: {"description":"Float (exp)","test":1e99999}
+  [!] EXPECTED: {"description": "Float (exp)", "test": 1E400}
+  [-] Result: {"description":"Float (exp)","test":1e99999}
 
-    ***/
-    return 0;
+  ***/
+  return 0;
 }
