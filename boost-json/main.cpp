f1ae95a9cc90f66b5675db19fbe7b054ddb95fd5
Randolph Ledesma <randopledesma+gitlab@hotmail.com>
2025-06-10 13:14:09 +0000

    + Includes program output of boost json

--- boost-json/main.cpp
+++ boost-json/main.cpp
@@ -11,5 +11,13 @@ int main(int argc, char **argv) {
     std::string real = serialize(test);
     std::cout << "[!] EXPECTED: " << experiment << std::endl;
     std::cout << "[-] Result: " << real << std::endl;
+
+    /***
+     * program output:
+
+    [!] EXPECTED: {"description": "Float (exp)", "test": 1E400}
+    [-] Result: {"description":"Float (exp)","test":1e99999}
+
+    ***/
     return 0;
 }
