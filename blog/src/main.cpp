79f540fd7486e95c8ce6fcb206cd970697961de9
Randolph Ledesma <randopledesma+gitlab@hotmail.com>
2025-06-11 15:38:05 +0000

    + Includes demo of cmark_markdown_to_html()

--- blog/src/main.cpp
+++ blog/src/main.cpp
@@ -1,5 +1,6 @@
 #include "include/connectionPool.hpp"
 #include "include/dbConnection.hpp"
+#include <cmark.h>
 #include <iostream>
 #include <memory>
 #include <string_view>
@@ -13,17 +14,19 @@ int main(int argc, char **argv) {
 
   try {
     // Initialize connection pool
-    auto pool = db::ConnectionPool(
-        "dbname=mydb user=myuser password=mypassword host=192.168.100.3 port=5432",
-        MAX_DB_CONNECTION);
+    auto pool =
+        db::ConnectionPool("dbname=mydb user=myuser password=mypassword "
+                           "host=192.168.100.3 port=5432",
+                           MAX_DB_CONNECTION);
 
     // Get a connection from the pool
     auto conn = pool.getConnection();
     if (conn) {
       std::cout << "DB pool connection: OK" << std::endl;
       pqxx::work txn(*conn);
-      auto result = txn.exec("SELECT * FROM posts WHERE id = $1 LIMIT 1", postId);
-      for (const auto& row : result) {
+      auto result =
+          txn.exec("SELECT * FROM posts WHERE id = $1 LIMIT 1", postId);
+      for (const auto &row : result) {
         std::cout << "ID: " << row[0].as<int>()
                   << ", Title: " << row[1].as<std::string>() << "\n";
       }
@@ -32,6 +35,13 @@ int main(int argc, char **argv) {
     } else {
       std::cerr << "Failed to get connection from pool" << std::endl;
     }
+
+    // Convert Markdown to HTML
+    const char *markdown = "# Hello, World!\nThis is a **Markdown** example.";
+    auto html = std::unique_ptr<char, void (*)(void *)>(
+        cmark_markdown_to_html(markdown, strlen(markdown), CMARK_OPT_DEFAULT),
+        std::free);
+    std::cout << "Markdown to HTML:\n" << html.get() << std::endl;
 
   } catch (const std::exception &e) {
     std::cerr << "Error: " << e.what() << "\n";
