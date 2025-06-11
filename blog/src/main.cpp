308e6cf2c96d5a08483c65dbf3d296a61027ccbf
Randolph Ledesma <randopledesma+gitlab@hotmail.com>
2025-06-11 17:32:20 +0000

    + Refactors variables

--- blog/src/main.cpp
+++ blog/src/main.cpp
@@ -65,21 +65,20 @@ int main(int argc, char **argv) {
 
   auto const address = net::ip::make_address("0.0.0.0");
   auto const port = static_cast<unsigned short>(8181);
-  auto const doc_root = std::make_shared<std::string>("/tmp");
-  auto const threads = std::max<int>(1, 4);
+  auto const docRoot = std::make_shared<std::string>("/tmp");
+  auto const threadCount = std::max<int>(1, 4);
 
   // The io_context is required for all I/O
-  net::io_context ioc{threads};
+  net::io_context ioc{threadCount};
 
   // Create and launch a listening port
-  std::make_shared<listener>(ioc, tcp::endpoint{address, port}, doc_root)
-      ->run();
+  std::make_shared<listener>(ioc, tcp::endpoint{address, port}, docRoot)->run();
 
   // Run the I/O service on the requested number of threads
-  std::vector<std::thread> v;
-  v.reserve(threads - 1);
-  for (auto i = threads - 1; i > 0; --i) {
-    v.emplace_back([&ioc] { ioc.run(); });
+  std::vector<std::thread> threads;
+  threads.reserve(threadCount - 1);
+  for (auto i = threadCount - 1; i > 0; --i) {
+    threads.emplace_back([&ioc] { ioc.run(); });
   }
   ioc.run();
 
