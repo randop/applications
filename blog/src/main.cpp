e4a1db040caaf8407f2eb0e5e2c2b7de1c44fb2d
Randolph Ledesma <randopledesma+gitlab@hotmail.com>
2025-06-11 17:25:08 +0000

    + Boots http server running at port 8181 on blog project

--- blog/src/main.cpp
+++ blog/src/main.cpp
@@ -63,5 +63,25 @@ int main(int argc, char **argv) {
     return EXIT_FAILURE;
   }
 
+  auto const address = net::ip::make_address("0.0.0.0");
+  auto const port = static_cast<unsigned short>(8181);
+  auto const doc_root = std::make_shared<std::string>("/tmp");
+  auto const threads = std::max<int>(1, 4);
+
+  // The io_context is required for all I/O
+  net::io_context ioc{threads};
+
+  // Create and launch a listening port
+  std::make_shared<listener>(ioc, tcp::endpoint{address, port}, doc_root)
+      ->run();
+
+  // Run the I/O service on the requested number of threads
+  std::vector<std::thread> v;
+  v.reserve(threads - 1);
+  for (auto i = threads - 1; i > 0; --i) {
+    v.emplace_back([&ioc] { ioc.run(); });
+  }
+  ioc.run();
+
   return EXIT_SUCCESS;
 }
