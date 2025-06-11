531a95ea0d7980caeb51525cec2e2d617605421a
Randolph Ledesma <randopledesma+gitlab@hotmail.com>
2025-06-11 15:45:30 +0000

    + Updates database migration init script to insert first post

--- blog/resources/database/init.sql
+++ blog/resources/database/init.sql
@@ -2,7 +2,10 @@ CREATE TABLE IF NOT EXISTS posts (
     id SERIAL PRIMARY KEY,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    title VARCHAR(255) NOT NULL
+    title VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL
 );
 
 GRANT ALL PRIVILEGES ON TABLE posts TO myuser;
+
+INSERT INTO posts (id, title, content) VALUES (1, 'My first post', '# Hello, World!\nThis is a **Markdown** example.');
