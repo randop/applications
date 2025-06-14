e23199277fa33c248fe1c783a105129e61ed4b9d
Randolph <randopledesma+gitlab@hotmail.com>
2025-06-14 01:35:48 +0000

    + Refactor blog project init migration + Add seed migration

--- blog/resources/database/002-seed.sql
+++ blog/resources/database/002-seed.sql
@@ -1,34 +1,3 @@
-ALTER DATABASE mydb
-SET
-    timezone TO 'UTC';
-
-CREATE TABLE IF NOT EXISTS modes (
-    id serial PRIMARY KEY,
-    created_at timestamp DEFAULT current_timestamp,
-    updated_at timestamp DEFAULT current_timestamp,
-    mode varchar(25) NOT NULL
-);
-
-INSERT INTO
-    modes (id, mode)
-VALUES
-    (1, 'markdown'),
-    (2, 'html');
-
-GRANT ALL PRIVILEGES ON TABLE modes TO myuser;
-
-CREATE TABLE IF NOT EXISTS posts (
-    id SERIAL PRIMARY KEY,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    title VARCHAR(255) NOT NULL,
-    content TEXT NOT NULL,
-    mode_id INTEGER NOT NULL DEFAULT 1,
-    CONSTRAINT fk_mode_id FOREIGN KEY (mode_id) REFERENCES modes (id)
-);
-
-GRANT ALL PRIVILEGES ON TABLE posts TO myuser;
-
 INSERT INTO
     posts (
         id,
