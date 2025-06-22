7c65c158daa94ee967e7aa8f0290867e2fe05f99
Randolph <randopledesma+gitlab@hotmail.com>
2025-06-22 15:41:48 +0000

    + Update layout header value

--- blog/resources/database/seed.js
+++ blog/resources/database/seed.js
@@ -17,10 +17,27 @@ db.modes.insertMany([
 // Create layouts
 //*****************************************************************************
 db.createCollection("layouts");
+const defaultHeaderValue = `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>%REPLACE_WITH_TITLE_ID%</title>
+
+    <!-- SEO Meta Tags -->
+    <meta name="description" content="Randolph Ledesma">
+    <meta name="keywords" content="Randolph Ledesma">
+    <meta name="author" content="Randolph Ledesma">
+    <meta name="robots" content="index, follow">
+
+    <meta name="theme-color" content="#ffffff">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  </head>
+  <body>`;
 // Create a unique index on the 'id' field
 db.layouts.createIndex({ id: 1 }, { unique: true });
 db.layouts.insertMany([
-  { id: 1, header: "<html><body>", footer: "</body></html>" },
+  { id: 1, header: defaultHeaderValue, footer: "</body></html>" },
 ]);
 
 //*****************************************************************************
