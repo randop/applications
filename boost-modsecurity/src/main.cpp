c8ab9a162872f5249f81d5f178007e00acff656a
Randolph <randopledesma+gitlab@hotmail.com>
2025-06-28 13:08:34 +0000

    + Implement main function for Boost ModSecurity project

---
+++ boost-modsecurity/src/main.cpp
@@ -0,0 +1,30 @@
+#include "httpServer.hpp"
+#include <boost/asio.hpp>
+#include <iostream>
+#include <thread>
+
+int main(int argc, char* argv[]) {
+  try {
+    const auto num_threads = std::max(1u, std::thread::hardware_concurrency());
+    const std::string address = "0.0.0.0";
+    const unsigned short port = 8080;
+
+    boost::asio::io_context ioc{static_cast<int>(num_threads)};
+    HttpServer server(ioc, address, port);
+    server.Start();
+
+    std::vector<std::thread> threads;
+    threads.reserve(num_threads);
+    for (auto i = num_threads; i > 0; --i) {
+      threads.emplace_back([&ioc] { ioc.run(); });
+    }
+
+    for (auto& thread : threads) {
+      thread.join();
+    }
+  } catch (const std::exception& e) {
+    std::cerr << "Error: " << e.what() << '\n';
+    return 1;
+  }
+  return 0;
+}
