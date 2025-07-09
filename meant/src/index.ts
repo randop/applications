4a02ecb5ff559a29cd41ffe44f39ae6f00552be0
Randolph <randopledesma+gitlab@hotmail.com>
2025-07-09 05:14:48 +0000

    Put index route handler

--- meant/src/index.ts
+++ meant/src/index.ts
@@ -1,6 +1,22 @@
 const log = console;
-log.info("index.ts");
 
-let a: string = "World";
-a = 1;
-log.info(`Hello: ${a}`);
+import * as express from "express";
+import { Application, Router, Request, Response } from "express";
+
+const app: Application = express();
+const PORT: number = 3000;
+const HOST: string = "0.0.0.0";
+
+app.use(express.json());
+
+const router = Router();
+
+router.get("/", async (req: Request, res: Response) => {
+  res.send({ data: "Hello World!", request: req.url });
+});
+
+app.use(router);
+
+app.listen(PORT, HOST, () => {
+  log.info(`Server running on http://${HOST}:${PORT}`);
+});
