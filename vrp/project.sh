912ebd97c521f1617cb37e2c7a07734758de7d9c
Randolph <randopledesma+gitlab@hotmail.com>
2025-09-16 12:07:21 +0000

    Initialize Vibe Resource Planning (VRP) project

---
+++ vrp/project.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+# Initialize client angular sub project
+pnpm create @angular client --standalone --style=css --routing
+
+# ✔ Do you want to enable Server-Side Rendering (SSR) and Static Site Generation (SSG/Prerendering)? No
+# ✔ Do you want to create a 'zoneless' application without zone.js? Yes
+# ✔ Which AI tools do you want to configure with Angular best practices? https://angular.dev/ai/develop-with-ai None
+# CREATE client/README.md (1469 bytes)
+# CREATE client/.editorconfig (314 bytes)
+# CREATE client/.gitignore (587 bytes)
+# CREATE client/angular.json (2276 bytes)
+# CREATE client/package.json (1103 bytes)
+# CREATE client/tsconfig.json (992 bytes)
+# CREATE client/tsconfig.app.json (429 bytes)
+# CREATE client/tsconfig.spec.json (408 bytes)
+# CREATE client/.vscode/extensions.json (130 bytes)
+# CREATE client/.vscode/launch.json (470 bytes)
+# CREATE client/.vscode/tasks.json (938 bytes)
+# CREATE client/src/main.ts (222 bytes)
+# CREATE client/src/index.html (292 bytes)
+# CREATE client/src/styles.css (80 bytes)
+# CREATE client/src/app/app.css (0 bytes)
+# CREATE client/src/app/app.spec.ts (780 bytes)
+# CREATE client/src/app/app.ts (288 bytes)
+# CREATE client/src/app/app.html (20122 bytes)
+# CREATE client/src/app/app.config.ts (383 bytes)
+# CREATE client/src/app/app.routes.ts (77 bytes)
+# CREATE client/public/favicon.ico (15086 bytes)
+# ✔ Packages installed successfully.
+# hint: Using 'master' as the name for the initial branch. This default branch name
+# hint: is subject to change. To configure the initial branch name to use in all
+# hint: of your new repositories, which will suppress this warning, call:
+# hint:
+# hint:   git config --global init.defaultBranch <name>
+# hint:
+# hint: Names commonly chosen instead of 'master' are 'main', 'trunk' and
+# hint: 'development'. The just-created branch can be renamed via this command:
+# hint:
+# hint:   git branch -m <name>
+#     Successfully initialized git.
+#
