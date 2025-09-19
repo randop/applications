65b50e8087f36d01ef6fe8fc25406293e2ee8240
Randolph <randopledesma+gitlab@hotmail.com>
2025-09-19 07:08:16 +0000

    Implement database module using mongodb on VRP project

--- vrp/server/src/app.module.ts
+++ vrp/server/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { TasksController } from './tasks/tasks.controller';
+import { AppController } from '@/app.controller';
+import { AppService } from '@/app.service';
+import { TasksController } from '@/tasks/tasks.controller';
+import { DatabaseModule } from '@/database/database.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule],
   controllers: [AppController, TasksController],
   providers: [AppService],
 })
--- vrp/server/src/tasks/tasks.controller.ts
+++ vrp/server/src/tasks/tasks.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get, Param } from '@nestjs/common';
-import { AppService } from '../app.service';
+import { AppService } from '@/app.service';
 
 @Controller('tasks')
 export class TasksController {
