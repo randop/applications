5de867eed4da399097a2104132ebe04aa1c49f42
Randolph Ledesma <randop@me.com>
2025-11-25 05:19:52 +0000

    Put checksum-correct NMEA byte stream for SM North EDSA, Philippines

--- nmea-gps/main.cpp
+++ nmea-gps/main.cpp
@@ -15,14 +15,13 @@ int main() {
   std::cout << "NMEA-GPS" << std::endl;
 
   const char bytestream[] =
-        "$GPGGA,092725.00,4807.038,N,01131.000,E,1,08,0.9,545.4,M,46.9,M,,*47\r\n"  // Valid: XOR=71='G'
-        "$GPRMC,092725.00,A,4807.038,N,01131.000,E,022.4,084.4,230394,003.1,W*6A\r\n" // Valid: XOR=106='j'
-        "$GPGSA,A,3,04,05,,09,12,,,24,25,,30,,3.6,2.1,2.0*3C\r\n"                   // Valid: XOR=60='<'
-        "$GPGSV,2,1,08,01,40,083,46,02,17,308,41,12,07,344,39,14,22,228,40*75\r\n"   // Valid: XOR=117='u'
-        "$GPGSV,2,2,08,16,19,256,25,22,11,301,19,29,08,080,00,30,05,180,00*71\r\n"   // Valid: XOR=113='q'
-        "$GPGGA,123519,4807.038,N,01131.000,E,1,08,0.9,545.4,M,46.9,M,,*47\r\n"      // Valid: XOR=71='G'
-        "$GPRMC,123519,A,4807.038,N,01131.000,E,041.0,090.0,251194,003.1,W*6B\r\n"    // Valid: XOR=107='k'
-        "\r\n";  // Trailing empty line (parser ignores)
+    "$GPGGA,143528.00,1439.42000,N,12101.62000,E,1,09,0.92,48.2,M,48.2,M,,*69\r\n"
+    "$GPRMC,143528.00,A,1439.42000,N,12101.62000,E,0.12,87.34,251125,,,A*62\r\n"
+    "$GPGSA,A,3,01,03,05,09,12,17,19,23,28,,,,3.2,1.8,2.6*36\r\n"
+    "$GPGSV,3,1,12,01,45,120,38,03,35,280,34,05,60,060,42,09,25,310,36*72\r\n"
+    "$GPGSV,3,2,12,12,55,180,40,17,30,050,37,19,70,240,44,23,15,090,32*75\r\n"
+    "$GPGSV,3,3,12,28,40,320,39,31,20,160,33,32,10,200,30,33,05,340,28*71\r\n"
+    "\r\n";
 
   NMEAParser parser;
   GPSService gps(parser);
@@ -40,7 +39,7 @@ int main() {
       cout << " # Searching..." << endl;
     }
   };
-  // Send in a log file or a byte stream
+
   try {
     parser.readBuffer((uint8_t *)bytestream, sizeof(bytestream));
   } catch (NMEAParseError &err) {
@@ -48,17 +47,14 @@ int main() {
   }
 
   string line;
-	ifstream file("nmea_log.txt");
-	while (getline(file, line)){
-		try {
-			parser.readLine(line);
-		}
-		catch (NMEAParseError& e){
-			cout << e.message << endl << endl;
-			// You can keep feeding data to the gps service...
-			// The previous data is ignored and the parser is reset.
-		}
-	}
+  ifstream file("nmea_log.txt");
+  while (getline(file, line)) {
+    try {
+      parser.readLine(line);
+    } catch (NMEAParseError &e) {
+      cout << e.message << endl << endl;
+    }
+  }
 
   return EXIT_SUCCESS;
 }
