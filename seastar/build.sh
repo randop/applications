0d8f1200cf201c026224be7b20ac9988e1fbfb09
Randolph <randopledesma+gitlab@hotmail.com>
2026-04-15 17:56:43 +0000

    project(seastar): Develop barebones C++23 seastar demo program

---
+++ seastar/build.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+set -eu
+
+export OPT_PREFIX="$HOME/opt"
+export PKG_CONFIG_PATH="$HOME/.local/lib/pkgconfig"
+export BOOST_ROOT="$HOME/opt/boost/current"
+export CMAKE_PREFIX_PATH="$HOME/opt/seastar/current"
+export CMAKE_PREFIX_PATH="${OPT_PREFIX}/boost/current:$CMAKE_PREFIX_PATH"
+export CMAKE_PREFIX_PATH="${OPT_PREFIX}/fmt/current:${CMAKE_PREFIX_PATH}"
+export CMAKE_PREFIX_PATH="${OPT_PREFIX}/hwloc/current:${CMAKE_PREFIX_PATH}"
+export CMAKE_PREFIX_PATH="${OPT_PREFIX}/protobuf/current:${CMAKE_PREFIX_PATH}"
+export CMAKE_PREFIX_PATH="${OPT_PREFIX}/lksctp/current:${CMAKE_PREFIX_PATH}"
+export CMAKE_PREFIX_PATH="${OPT_PREFIX}/colm-suite/current:${CMAKE_PREFIX_PATH}"
+export CMAKE_PREFIX_PATH="${OPT_PREFIX}/valgrind/current:${CMAKE_PREFIX_PATH}"
+
+echo "CMAKE_PREFIX_PATH: $CMAKE_PREFIX_PATH"
+
+mkdir -p build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release
+make -j$(nproc)
