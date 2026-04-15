50160fd9ac8ba566585b5f646bb60dc76bc05807
Randolph <randopledesma+gitlab@hotmail.com>
2026-04-15 18:03:41 +0000

    project(seastar): Enhancements on demo program

--- seastar/main.cc
+++ seastar/main.cc
@@ -1,14 +1,76 @@
 #include <seastar/core/app-template.hh>
-#include <seastar/core/seastar.hh>
+#include <seastar/core/reactor.hh>
+#include <seastar/core/future-util.hh>
+#include <seastar/net/api.hh>
 #include <seastar/util/log.hh>
+#include <seastar/core/loop.hh>
+#include <seastar/core/do_with.hh>
 
-using namespace seastar;
-logger applog("app");
+namespace bpo = boost::program_options;
+static seastar::logger applog("tcp-echo");
+
+seastar::future<> handle_connection(seastar::connected_socket sock, seastar::socket_address addr) {
+    applog.info("Accepted connection from {}", addr);
+
+    auto in  = sock.input();
+    auto out = sock.output();
+
+    return seastar::do_with(
+        std::move(in), std::move(out),
+        [](auto& in, auto& out) -> seastar::future<> {
+            return seastar::keep_doing([&in, &out]() -> seastar::future<> {
+                // Read up to the stream's internal buffer size (usually efficient)
+                return in.read().then([&out](seastar::temporary_buffer<char> buf) {
+                    if (buf.empty()) {
+                        // Client closed connection
+                        return seastar::make_ready_future<>();
+                    }
+
+                    // Echo back exactly what we received
+                    return out.write(std::move(buf)).then([&out] {
+                        return out.flush();
+                    });
+                });
+            }).finally([&in, &out] {
+                return out.close().then([&in] {
+                    return in.close();
+                });
+            });
+        });
+}
+
+seastar::future<> service_loop(uint16_t port) {
+    seastar::listen_options lo;
+    lo.reuse_address = true;
+
+    return seastar::do_with(
+        seastar::listen(seastar::make_ipv4_address({port}), lo),
+        [port](auto& listener) {
+            applog.info("TCP Echo server listening on port {}", port);
+
+            return seastar::keep_doing([&listener] {
+                return listener.accept().then([](seastar::accept_result res) {
+                    (void)handle_connection(std::move(res.connection), res.remote_address);
+                });
+            });
+        });
+}
 
 int main(int argc, char** argv) {
     seastar::app_template app;
-    app.run(argc, argv, [] () -> future<> {
-        applog.info("Hello world!");
-        return make_ready_future<>();
+
+    app.add_options()
+                    ("port,p", bpo::value<uint16_t>()->default_value(10000), "Server port");
+
+    return app.run(argc, argv, [&app] {
+        auto& cfg = app.configuration();
+        uint16_t port = cfg["port"].as<uint16_t>();
+
+        return seastar::smp::invoke_on_all([port] {
+            return service_loop(port);
+        }).handle_exception([](std::exception_ptr ep) {
+            applog.error("Fatal error: {}", ep);
+            return seastar::make_exception_future<>(ep);
+        });
     });
 }
