066fda3692cb9ef0b7720debced56e4f39da1c2e
Randolph <randopledesma+gitlab@hotmail.com>
2026-04-17 10:01:46 +0000

    project(seastar): Concurrent TCP server

---
+++ seastar/tcp-server.cc
@@ -0,0 +1,111 @@
+#include <seastar/core/app-template.hh>
+#include <seastar/core/coroutine.hh>
+#include <seastar/core/file.hh>
+#include <seastar/core/future.hh>
+#include <seastar/core/gate.hh>
+#include <seastar/core/reactor.hh>
+#include <seastar/core/sharded.hh>
+#include <seastar/net/api.hh>
+#include <seastar/util/log.hh>
+#include <seastar/util/tmp_file.hh>
+
+#include "stop_signal.hh"
+
+#include <chrono>
+#include <exception>
+#include <memory>
+#include <random>
+#include <utility>
+
+static seastar::logger applog("concurrent-demo");
+
+seastar::future<> handle_connection(seastar::connected_socket cs,
+                                    seastar::socket_address remote) {
+  applog.info("+ connection from {}", remote);
+  return seastar::tmp_dir::do_with(
+      [cs = std::move(cs),
+       remote](seastar::tmp_dir &t) mutable -> seastar::future<> {
+        auto in = cs.input();
+        auto out = cs.output();
+        auto seed = std::chrono::system_clock::now().time_since_epoch().count();
+        auto rnd = std::mt19937(seed);
+        seastar::sstring client_filename =
+            (t.get_path() / std::to_string(rnd())).native();
+        auto f = co_await seastar::open_file_dma(
+            client_filename,
+            seastar::open_flags::rw | seastar::open_flags::create);
+        applog.info("Writing to {} for client {}", client_filename, remote);
+        try {
+          size_t offset = 0;
+          while (true) {
+            auto buf = co_await in.read();
+            if (buf.empty()) {
+              break;
+            }
+            co_await f.dma_write(offset, buf.get(), buf.size());
+            offset += buf.size();
+          }
+          applog.info("Finished writing {} bytes to {} for client {}", offset,
+                      client_filename, remote);
+        } catch (const std::exception &ex) {
+          applog.warn("! connection from {} error: {}", remote, ex.what());
+        }
+        co_await f.close();
+        co_await out.close();
+      });
+}
+
+seastar::future<> serve(uint16_t port,
+                        const seastar_apps_lib::stop_signal &stop_signal,
+                        seastar::gate &gate) {
+  seastar::listen_options opts;
+  opts.reuse_address = true;
+  auto ss = seastar::listen(seastar::make_ipv4_address({port}), opts);
+  applog.info("Echo server shard {} listening on 0.0.0.0:{}",
+              seastar::this_shard_id(), port);
+  while (!stop_signal.stopping()) {
+    try {
+      auto ar = co_await ss.accept();
+      auto addr = ar.remote_address;
+      gate.enter();
+      (void)handle_connection(std::move(ar.connection), addr)
+          .handle_exception([=](std::exception_ptr ep) {
+            try {
+              std::rethrow_exception(ep);
+            } catch (const std::exception &ex) {
+              applog.error("Unhandled error for {}: {}", addr, ex.what());
+            }
+          })
+          .finally([&gate] { gate.leave(); });
+    } catch (const std::exception &ex) {
+      if (!stop_signal.stopping()) {
+        applog.error("Accept failed: {}", ex.what());
+      }
+    }
+  }
+  ss.abort_accept();
+  applog.info("Waiting for connections to finish on shard {}",
+              seastar::this_shard_id());
+  co_await gate.close();
+  applog.info("Server shard {} stopped", seastar::this_shard_id());
+}
+
+int main(int argc, char **argv) {
+  seastar::app_template app;
+  namespace po = boost::program_options;
+  app.add_options()("port,p", po::value<uint16_t>()->default_value(8080),
+                    "TCP port to listen on");
+  return app.run(argc, argv, [&app]() -> seastar::future<> {
+    uint16_t port = app.configuration()["port"].as<uint16_t>();
+    seastar::sharded<seastar::gate> gate;
+    co_await gate.start();
+    auto stop_signal = std::make_shared<seastar_apps_lib::stop_signal>();
+    auto f = seastar::smp::invoke_on_all([port, stop_signal, &gate] {
+      return serve(port, *stop_signal, gate.local());
+    });
+    co_await stop_signal->wait();
+    applog.info("Signal received, stopping servers");
+    co_await std::move(f);
+    co_await gate.stop();
+  });
+}
