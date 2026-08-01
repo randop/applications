61045a1475778d944c49b0d3ff36bc08def2eace
Randolph Ledesma <randopledesma+gitlab@hotmail.com>
2026-08-01 11:46:17 +0000

    vibecoding(dataman): Build JPA data manager using spring boot with Java 25

---
+++ vibecoding/spring-boot/dataman/src/main/java/com/tindango/dataman/DatamanApplication.java
@@ -0,0 +1,13 @@
+package com.tindango.dataman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatamanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatamanApplication.class, args);
+	}
+
+}
---
+++ vibecoding/spring-boot/dataman/src/test/java/com/tindango/dataman/DatamanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tindango.dataman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatamanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
