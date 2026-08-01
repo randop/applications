750718100b16e75b5ae0e6ae082db68639cb04c4
Randolph Ledesma <randopledesma+gitlab@hotmail.com>
2026-08-01 11:37:01 +0000

    vibecoding(api): Build backend API using spring boot with Java 25

---
+++ vibecoding/spring-boot/api/src/test/java/com/tindango/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tindango.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
