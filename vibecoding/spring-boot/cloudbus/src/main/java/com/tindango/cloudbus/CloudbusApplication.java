ddac2458961d5b8a285bf5b3d158416cf1e5514f
Randolph Ledesma <randopledesma+gitlab@hotmail.com>
2026-08-01 11:40:19 +0000

    vibecoding(cloudbus): Build kafka messaging using spring boot with Java 25

---
+++ vibecoding/spring-boot/cloudbus/src/main/java/com/tindango/cloudbus/CloudbusApplication.java
@@ -0,0 +1,13 @@
+package com.tindango.cloudbus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudbusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudbusApplication.class, args);
+	}
+
+}
---
+++ vibecoding/spring-boot/cloudbus/src/test/java/com/tindango/cloudbus/CloudbusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tindango.cloudbus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudbusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
