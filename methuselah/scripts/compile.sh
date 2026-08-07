df4945abda5f7d26f7aee6cef0ab3b5d28b8b270
Randolph Ledesma <randopledesma+gitlab@hotmail.com>
2026-08-07 19:05:59 +0000

    project(methuselah): fix boost detection during build setup

--- methuselah/scripts/compile.sh
+++ methuselah/scripts/compile.sh
@@ -25,6 +25,19 @@ if [ -n "${PKG_CONFIG_PATH+set}" ]; then
 else
   export PKG_CONFIG_PATH=$LOCAL_PKGCONFIG
 fi
+if [ ! -f "${OPT_PREFIX}/boost/current/lib/libboost_atomic.so" ]; then
+  echo "boost: missing!!!"
+fi
+BOOST_VERSION=v1.91.0
+BOOST_STRING=boost-1.91.0-1
+export BOOST_ROOT=${OPT_PREFIX}/boost/current
+
+if [ -n "${CMAKE_PREFIX_PATH+set}" ]; then
+  CMAKE_PREFIX_PATH="${OPT_PREFIX}/boost/current:${CMAKE_PREFIX_PATH}"
+else
+  CMAKE_PREFIX_PATH="${OPT_PREFIX}/boost/current"
+fi
+export CMAKE_PREFIX_PATH
 
 if [ ! -f "subprojects/raygui/src/raygui.h" ]; then
   mkdir -p subprojects/raygui
