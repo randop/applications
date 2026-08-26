d66bf688cd9d375a950cda1d8710faba1a518ab3
Randolph Ledesma <randopledesma+gitlab@hotmail.com>
2026-08-26 09:19:03 +0000

    project(shield): develop smtp mail protection spring boot application

---
+++ java/shield/src/test/java/com/quizbin/mail/shield/ShieldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quizbin.mail.shield;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShieldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
